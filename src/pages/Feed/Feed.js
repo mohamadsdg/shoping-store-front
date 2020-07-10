9b9d463a3871c7a7f98f8627e256201b05885244
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-07-11 00:27:42 +0430

    delete post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -178,7 +178,9 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('URL')
+    fetch(`http://127.0.0.1:8080/feed/post/${postId}`,{
+      method: 'DELETE',
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
