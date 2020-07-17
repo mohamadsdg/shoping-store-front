d72fcaf6b4ac57d15e2bcf9617f7988729fd33a8
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-07-17 22:10:29 +0430

    add status

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,7 +22,11 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://127.0.0.1:8080/auth/status',{
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -78,7 +82,16 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://127.0.0.1:8080/auth/status',{
+      method:'PATCH',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        status: this.state.status
+      })
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
@@ -215,7 +228,6 @@ class Feed extends Component {
   };
 
   catchError = error => {
-    console.log(error)
     this.setState({ error: error });
   };
 
