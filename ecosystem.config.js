c3d021a5f7c01eeaaabdabefc53ec74e0fe820dd
mohamadsdg <mohamad.r.sadeghi93@gmail.com>
2020-07-24 20:22:27 +0430

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+       {
+          name: 'app',
+          script: 'build/server.js',
+          instances: 'max',
+          env: {
+             NODE_ENV: 'development',
+          },
+          env_production: {
+             NODE_ENV: 'production',
+          },
+       },
+    ],
+ };
+ 
